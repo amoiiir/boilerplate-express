a321ef3b087e504b217e740235fa0ff9dfe19ac5
Amir Hamzah <mahamzah535@gmail.com>
2024-12-19 14:48:53 +0800

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Basic Node and Express
 
 This is the boilerplate code for the Basic Node and Express Challenges. Instructions for working on these challenges start at https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/
+
+## Env File
+-  a hidden file that is used to pass environment variables to your application.
+- The environment variables are accessible from the app as process.env.VAR_NAME
+
+## Middleware functions
+- What is that?
+- request handler that allows intercepts and manipulates incoming requests
+
+- functions that take 3 arguments: 
+  - request object (req)
+  - response object (res)
+  - next function in the application’s request-response cycle (routing)
+- this function can cause side effect to the application
+  - usually add information to the request or object response
+
+So what is the function of the middleware?
+- make changes to the request and the response objects
+- end the request-response cycle
+- call the next middleware function in the stack if there is one
--- myApp.js
+++ myApp.js
@@ -1,39 +1,81 @@
-let express = require('express');
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+let express = require("express");
 let app = express();
 
+//configure dotenv
+dotenv.config();
+
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
+// middleware functions -> simple logger
+app.use(function middleware(req, res, next){
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
 
+app.get("/now", function middleware(req, res, next){
+  req.time = new Date().toString();
+  console.log("time", req.time);
+  next();
+}, function(req, res){
+  res.send({
+    time: req.time,
+  });
+});
 
+//get route parameter from input client
+// app.get("/:word/echo", function middleware(req, res, next){
+//   res.send({ echo: req.params.word});
+//   next();
+// });
 
+//get query parameter fromm input client
+app.get("/name", (req, res) => {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  
+  res.json({
+    name: `${firstName} ${lastName}`,
+  })
+});
 
+app.post("/name", function(req, res) {
+  // Handle the data in the request
+  var string = req.body.first + " " + req.body.last;
+  res.json({ name: string });
+});
 
+//What is the function of this app.get?
+// use get HTTP method to get response
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+// what is the function of the res.json?
+// 1. converts javascript object into JSON string -> basicly buat API
+// 2. set the appropriate response headers to tell client that response type is application/json
+// 3. sends the JSON as string as HTTP response
+app.get("/json", (req, res) => {
+  //access the env variables
+  const MESSAGE_STYLE = process.env.MESSAGE_STYLE;
+  console.log("message", MESSAGE_STYLE);
 
+  const responseMessage =
+    MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json";
 
+  res.json({
+    message: responseMessage,
+  });
+});
 
+// What is the function of the code below?
+// place the static assets needed by our website application (stylesheet, scripts, image)
+// it is a middleware function
+// it will apply for all requests
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
